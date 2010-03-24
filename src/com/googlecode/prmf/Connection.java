eea958c0e2760b3d145255226a19adfa5e8fb0c9
Miorel-Lucian Palii <mlpalii@gmail.com>
2010-03-23 20:19:56 -0400

    Connection class

---
+++ src/com/googlecode/prmf/Connection.java
@@ -0,0 +1,17 @@
+package com.googlecode.prmf;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Connection {
+	public static void main(String[] arg) throws UnknownHostException, IOException {
+		Socket s = new Socket("irc.freenode.net", 6667);
+		Scanner in = new Scanner(s.getInputStream());
+		while(in.hasNextLine()) {
+			String line = in.nextLine();
+			System.out.println(line);
+		}
+	}
+}
