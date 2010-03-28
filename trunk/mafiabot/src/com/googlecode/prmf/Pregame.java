98e13abe1c80642bfd02f5e1242e44c100ed7959
Walter Cornnell <hopfrog16@gmail.com>
2010-03-28 16:17:38 -0400

    created a getPlayerList() method that returns an ArrayList of the players
    git-svn-id: http://prmf.googlecode.com/svn@46 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/mafiabot/src/com/googlecode/prmf/Pregame.java
+++ trunk/mafiabot/src/com/googlecode/prmf/Pregame.java
@@ -8,6 +8,10 @@ public class Pregame {
 	
 	InputStream is;
 	String startName;
+	ArrayList<Player> players;
+	ArrayList<Role> townRoles;
+	ArrayList<Role> mafiaRoles;
+	ArrayList<Role> roles; 
 	
 	public Pregame(InputStream is, String startName)
 	{
@@ -15,75 +19,79 @@ public class Pregame {
 		this.startName = startName;
 	}
 
-		public void startGame()
+	public void startGame()
+	{
+		Scanner in = new Scanner(is);
+		players = new ArrayList<Player>();
+		townRoles = new ArrayList<Role>();
+		mafiaRoles = new ArrayList<Role>();
+		roles = new ArrayList<Role>();
+		
+		
+		//joining the game	
+		
+		while(true) //break when the game starts
 		{
-			Scanner in = new Scanner(is);
-			ArrayList<Player> players = new ArrayList<Player>();
-			ArrayList<Role> townRoles = new ArrayList<Role>();
-			ArrayList<Role> mafiaRoles = new ArrayList<Role>();
-			ArrayList<Role> roles = new ArrayList<Role>();
+			String line = in.nextLine();
+			String[] msg = line.split(" ", 4);
+			String user = "#UFPT";
 			
+			if(msg[0].indexOf("!")>1)
+				user = msg[0].substring(1,msg[0].indexOf("!"));
 			
-			//joining the game	
-			
-			while(true) //break when the game starts
+			String destination = msg[2];
+			if(msg[3].equals(":!start") && user.equals(startName))		
+				break;
+				
+			if(msg[3].equals(":!join"))
 			{
-				String line = in.nextLine();
-				String[] msg = line.split(" ", 4);
-				String user = "#UFPT";
-				
-				if(msg[0].indexOf("!")>1)
-					user = msg[0].substring(1,msg[0].indexOf("!"));
-				
-				String destination = msg[2];
-				if(msg[3].equals(":!start") && user.equals(startName))		
-					break;
-					
-				if(msg[3].equals(":!join"))
+				players.add(new Player(user));
+				Communicator.getInstance().sendMessage(destination, user + " has joined the game!");
+			}
+			
+			if(msg[3].equals(":!quit"))
+			{
+				for(int a = 0; a < players.size(); ++a)
 				{
-					players.add(new Player(user));
-					Communicator.getInstance().sendMessage(destination, user + " has joined the game!");
+					Player temp = players.get(a);
+					if(temp.name.equals(user))
+						players.remove(a);
 				}
-				
-				if(msg[3].equals(":!quit"))
-				{
-					for(int a = 0; a < players.size(); ++a)
-					{
-						Player temp = players.get(a);
-						if(temp.name.equals(user))
-							players.remove(a);
-					}
-				}	
-			}
-			
-			//assigning roles
-			int numMafia = players.size()/3;
-			//create the Mafia team
-			for(int a = 0; a < numMafia; ++a)
-			{
-				mafiaRoles.add(new Mafia(new MafiaTeam()));
-			}
-			
-			//create the Town team
-			for(int a = 0; a < (players.size() - numMafia); ++a)
-			{
-				townRoles.add(new Citizen(new Town()));
-			}
-			Collections.shuffle(mafiaRoles);
-			Collections.shuffle(townRoles);
-			for(int a = 0; a < numMafia; ++a)
-			{
-				roles.add(mafiaRoles.get(a));
-			}
-			for(int a = 0; a < players.size() - numMafia; ++a)
-			{
-				roles.add(townRoles.get(a));
-			}
-			for(int a = 0; a < players.size(); ++a)
-			{
-				players.get(a).role = roles.get(a);
-			}
-			
+			}	
 		}
+		
+		//assigning roles
+		int numMafia = players.size()/3;
+		//create the Mafia team
+		for(int a = 0; a < numMafia; ++a)
+		{
+			mafiaRoles.add(new Mafia(new MafiaTeam()));
+		}
+		
+		//create the Town team
+		for(int a = 0; a < (players.size() - numMafia); ++a)
+		{
+			townRoles.add(new Citizen(new Town()));
+		}
+		Collections.shuffle(mafiaRoles);
+		Collections.shuffle(townRoles);
+		for(int a = 0; a < numMafia; ++a)
+		{
+			roles.add(mafiaRoles.get(a));
+		}
+		for(int a = 0; a < players.size() - numMafia; ++a)
+		{
+			roles.add(townRoles.get(a));
+		}
+		for(int a = 0; a < players.size(); ++a)
+		{
+			players.get(a).role = roles.get(a);
+		}
+		
 	}
 
+	public ArrayList<Player> getPlayerList()
+	{
+		return players;
+	}
+}
