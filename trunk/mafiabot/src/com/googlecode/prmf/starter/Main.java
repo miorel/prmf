a8cbd9045ff4b3706a7e0994e8634459df06d24c
Rodrigo Salazar <rodrodsalazar@gmail.com>
2010-03-28 16:29:58 -0400

    Made connection.java only handle connection and not the flow of the game.
    git-svn-id: http://prmf.googlecode.com/svn@47 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/mafiabot/src/com/googlecode/prmf/starter/Main.java
+++ trunk/mafiabot/src/com/googlecode/prmf/starter/Main.java
@@ -1,9 +1,11 @@
 package com.googlecode.prmf.starter;
 
-// TODO this class seems useless
+import java.io.*;
+import java.util.*;
+
 public class Main {
 	static Connection conn;
-
+	static Scanner in;
 	public static void main(String[] arg) 
 	{
 		try
@@ -15,6 +17,42 @@ public class Main {
 			System.out.println("oops");
 		}
 		
+		Communicator.setPrintStream(conn.os);
 		
+		try
+		{
+		in = new Scanner(conn.soc.getInputStream());
+		}
+		catch(Exception e)
+		{
+			System.out.println("oops");
+		}
+		
+		conn.os.println("NICK MAFIABOT22");
+		//System.out.println("**ENTERED NICK");
+		conn.os.println("USER MAFIABOT22 12 * MAFIABOT22");
+		//System.out.println("**ENTERED USER LOGIN");
+		conn.os.println("JOIN #UFPT");
+		//System.out.println("**JOINED CHANNEL");
+		conn.os.println("PRIVMSG #UFPT :HELLO I AM MAFAIA BOTT");
+		
+		while(in.hasNextLine())
+		{
+			String line = in.nextLine();
+			
+			String[] msg = line.split(" ",4);
+			String user = "#UFPT";
+			
+			if(msg[0].indexOf("!")>1)
+				user = msg[0].substring(1,msg[0].indexOf("!"));
+			
+			
+			if(msg.length >= 4 && msg[3].toLowerCase().equals(":!mafia"))
+			{
+				
+			}
+			
+			System.out.println(line);
+		}
 	}
 }
