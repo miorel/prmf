07d48f617509e9d0eebc8fd0708379cbfb5190a8
Rodrigo Salazar <rodrodsalazar@gmail.com>
2010-03-29 15:30:53 -0400

    Created Night and its basic constructor. Game now makes Day and night object and calls basic constructors. Day now has startDay(), which creates timerThread.
    git-svn-id: http://prmf.googlecode.com/svn@61 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/mafiabot/src/com/googlecode/prmf/Game.java
+++ trunk/mafiabot/src/com/googlecode/prmf/Game.java
@@ -1,15 +1,28 @@
 package com.googlecode.prmf;
 
 public class Game {
-	Player[] players; // TODO this does nothing, it should probably do something
+	Player[] players;
 	String gameStarter;
+	int numMafia =1;
+	boolean dayStart = true;
 	
 	public Game(String gameStarter)	{
 		this.gameStarter = gameStarter;
+		
 	}
 
 	public void startGame() {
 		Pregame pregame = new Pregame(gameStarter);
 		pregame.startGame();
+		players = pregame.getPlayerList();
+		Day day = new Day(players);
+		Night night = new Night(players);
+	}
+	
+	public Game(String gameStarter, boolean dayStart, int numMafia)
+	{
+		this(gameStarter);
+		this.dayStart = dayStart;
+		this.numMafia = numMafia;
 	}
 }
