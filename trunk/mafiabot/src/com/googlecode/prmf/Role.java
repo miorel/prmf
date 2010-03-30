7298e45c97eb44e06313babc20a4c896692106d4
Joseph Thuemler <joethuemler@gmail.com>
2010-03-29 20:01:16 -0400

    added pong functionality, testing role assignment
    git-svn-id: http://prmf.googlecode.com/svn@77 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/mafiabot/src/com/googlecode/prmf/Role.java
+++ trunk/mafiabot/src/com/googlecode/prmf/Role.java
@@ -1,6 +1,7 @@
 package com.googlecode.prmf;
 public abstract class Role {
 	Team team; // TODO default visibility is almost as bad as public
+	String name;
 	
 	void nightAction(Player target) {
 		//TODO functionality
