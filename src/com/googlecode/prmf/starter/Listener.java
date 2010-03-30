9bd32cb5b5dd27730406a4956bba58031a7201b5
Rodrigo Salazar <rodrodsalazar@gmail.com>
2010-03-29 21:51:01 -0400

    TOTAL REDESIGN!@@$%^%^*LOL

---
+++ src/com/googlecode/prmf/starter/Listener.java
@@ -0,0 +1,6 @@
+package com.googlecode.prmf.starter;
+
+
+public interface Listener {
+	public void receiveLine(String line, InputThread inputThread);
+}
--- src/com/googlecode/prmf/starter/Main.java
+++ src/com/googlecode/prmf/starter/Main.java
@@ -2,10 +2,7 @@ package com.googlecode.prmf.starter;
 
 import java.util.Scanner;
 
-import com.googlecode.prmf.Game;
-
 public class Main {
-	static Connection conn;
 	static Scanner in;
 	static String channel = "#UFPT";
 	static String server = "irc.freenode.net";
@@ -13,53 +10,9 @@ public class Main {
 	
 	public static void main(String[] arg) 
 	{
-		try
-		{
-			Connection.makeConnection(server, port);
-		}
-		catch(Exception e)
-		{
-			System.out.println("oops");
-		}
-		
-		Communicator.getInstance().setPrintStream(Connection.ps);
-		
-		try
-		{
-			in = new Scanner(Connection.is);
-		}
-		catch(Exception e)
-		{
-			System.out.println("oops2");
-		}
-		
-		Connection.ps.println("NICK MAFIABOT22");
-		//System.out.println("**ENTERED NICK");
-		Connection.ps.println("USER MAFIABOT22 12 * MAFIABOT22");
-		//System.out.println("**ENTERED USER LOGIN");
-		Connection.ps.println("JOIN " + channel);
-		//System.out.println("**JOINED CHANNEL");
-		Communicator.getInstance().sendMessage(channel, "Hello, I am MafiaBot.");
-		
 		InputThread inputThread = new InputThread(server, port);
 		inputThread.start();
-		
-		while(in.hasNextLine()) 
-		{
-			String line = in.nextLine();
-			
-			String[] msg = line.split(" ",4);
-			String user = channel;
-			
-			if(msg[0].indexOf("!") > 1)
-				user = msg[0].substring(1,msg[0].indexOf("!"));
-			
-			if(msg.length >= 4 && msg[3].toLowerCase().equalsIgnoreCase(":!mafia"))
-			{
-				Communicator.getInstance().sendMessage(channel,"Mafia game started!");
-				Game game = new Game(user);
-				game.startGame();
-			}
-		}
+		MafiaListener mafiaListener = new MafiaListener();
+		inputThread.addListener(mafiaListener);
 	}
 }
