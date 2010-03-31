0852003b7c3d51922e296787b9d1539594703a24
Miorel-Lucian Palii <mlpalii@gmail.com>
2010-03-31 10:38:12 -0400

    A few TODOs. Deleted Communicator class because it's now useless.

--- src/com/googlecode/prmf/starter/Main.java
+++ src/com/googlecode/prmf/starter/Main.java
@@ -3,16 +3,15 @@ package com.googlecode.prmf.starter;
 import java.util.Scanner;
 
 public class Main {
-	static Scanner in;
-	static String channel = "#UFPT";
+	static Scanner in; //TODO remove this variable
+	static String channel = "#UFPT"; //TODO make this lowercase
 	static String server = "irc.freenode.net";
 	static int port = 6667;
 	
-	public static void main(String[] arg) 
-	{
+	public static void main(String[] arg) {
 		InputThread inputThread = new InputThread(server, port);
-		inputThread.start();
-		MafiaListener mafiaListener = new MafiaListener();
+		inputThread.start(); //TODO start the thread AFTER adding the listener
+		MafiaListener mafiaListener = new MafiaListener(); //TODO declare things as what they're used, not as what they are
 		inputThread.addListener(mafiaListener);
 	}
 }
