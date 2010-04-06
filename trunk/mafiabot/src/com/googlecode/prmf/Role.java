0bbedd13ee7c81f60225879c304658226fd2a608
Joseph Thuemler <joethuemler@gmail.com>
2010-04-05 23:42:00 -0400

    started framework for Night, small changes to Role and Mafia
    git-svn-id: http://prmf.googlecode.com/svn@118 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/mafiabot/src/com/googlecode/prmf/Role.java
+++ trunk/mafiabot/src/com/googlecode/prmf/Role.java
@@ -1,7 +1,8 @@
 package com.googlecode.prmf;
 public abstract class Role {
-	Team team; // TODO default visibility is almost as bad as public
-	String name;
+	private Team team; // TODO default visibility is almost as bad as public
+	private String name;
+	private Player target;
 	
 	void nightAction(Player target) {
 		//TODO functionality
