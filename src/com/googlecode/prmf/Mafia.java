545e29bf04bbb10e835c42a8452acf815e191acb
Joseph Thuemler <joethuemler@gmail.com>
2010-04-05 23:42:00 -0400

    started framework for Night, small changes to Role and Mafia

--- src/com/googlecode/prmf/Mafia.java
+++ src/com/googlecode/prmf/Mafia.java
@@ -2,17 +2,22 @@ package com.googlecode.prmf;
 
 // this class is poorly implemented
 // it's a bad idea to write code relying on it until it gets fixed
-@Deprecated
+
 class Mafia extends Role{
-	static Team team;
+	private Team team;
+	private String name;
+	private Player target;
 	public Mafia(Team nteam) {
 		name = "mafia";
 		team = nteam;
-		// TODO team is a class (i.e. static) variable
-		// whereas nteam is an instance variable
-		// this seems bad
+		target = null;
 	}
 	public void nightAction(Player target) {
-		target.isMafiaTarget = true;
+		this.target = target;
+	}
+	
+	public String getName()
+	{
+		return name;
 	}
 }
--- src/com/googlecode/prmf/Night.java
+++ src/com/googlecode/prmf/Night.java
@@ -5,17 +5,39 @@ import com.googlecode.prmf.starter.IOThread;
 
 public class Night implements MafiaGameState
 {
-	Player[] players;
+	private Player[] players;
+	private IOThread inputThread;
+	private boolean[] actionComplete;
 	
 	public Night(Player[] players, IOThread inputThread)
 	{
-		
+		this.players = players;
+		this.inputThread = inputThread;
+		actionComplete = new boolean[players.length];
 	}
+	
+//	TODO add a timer to night
 	
 	public boolean receiveMessage(String line, IOThread inputThread)
 	{
+		
 		// TODO why is the channel name hardcoded?
 		inputThread.sendMessage("#UFPT" , " WE ARE IN NIGHT");
+		
+		//TODO parse message, do appropriate action
+		return isNightOver()     ;
+	}
+	
+	public void introduction()
+	{
+		//TODO tell each player his night message: role, night action, teammates if mafia, etc
+	}
+	
+	public boolean isNightOver()
+	{
+		//TODO check if all actions are in
 		return false;
 	}
+	
+	
 }
