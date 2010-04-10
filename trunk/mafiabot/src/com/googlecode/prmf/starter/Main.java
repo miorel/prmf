2f6437e973d3aba9f01c6e661f527c61cee12ad3
Mauricio Bell'Albero <mauriciofmar3@ufl.edu>
2010-04-09 21:25:54 -0400

    git-svn-id: http://prmf.googlecode.com/svn@151 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/mafiabot/src/com/googlecode/prmf/starter/Main.java
+++ trunk/mafiabot/src/com/googlecode/prmf/starter/Main.java
@@ -2,12 +2,13 @@ package com.googlecode.prmf.starter;
 
 public class Main {
 	static String channel = "#ufpt";
+	static String botName = "MAFIABOT22";
 	static String server = "irc.freenode.net";
 	static int port = 6667;
 	
 	public static void main(String[] arg) {
-		IOThread inputOutputThread = new IOThread(server, port, "#ufpt");
-		Listener mafiaListener = new MafiaListener(); 
+		IOThread inputOutputThread = new IOThread(server, port, "#ufpt",botName);
+		Listener mafiaListener = new MafiaListener(botName);
 		PingListener pingListener = new PingListener();
 		inputOutputThread.addListener(mafiaListener);
 		inputOutputThread.addListener(pingListener);
