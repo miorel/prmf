7b1e7f535f5ce1816f8da769a4d993a0a51ab29d
Rodrigo Salazar <rodrodsalazar@gmail.com>
2010-05-23 00:31:22 -0400

    * craigcrawler.pl: Implemented basic spider method, which works somewhat! 	Problems: There are a TON of pages! All of 1 city took 10-15 minutes. Script terminated itself after completing it's first city. 	* ChangeLog: organized changelog
    git-svn-id: http://prmf.googlecode.com/svn@361 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/CraigCrawler/craigcrawler.pl
+++ trunk/CraigCrawler/craigcrawler.pl
@@ -2,27 +2,35 @@
 
 use warnings;
 use strict;
-
 use LWP::Simple;
 
 my $url = "http://craigslist.org";
-my $content = get($url);
-
-# Are these 'global' variables?
-# These are global variables in the sense that you can access them anywhere
-# within this file. However, if you had multiple files they wouldn't be
-# accessible there. For true global variables use "our" instead of "my".
-my $pattern = "<a href=\"(http:\/\/[^\"]*\.craigslist\.org\/[^\"]*)\">([^<]*)<\/a>";
-my %links = ();
+my $pattern = "<a href=\"(http:\/\/[^\"]*\.craigslist\.org\/(?!about)[^\"]*)\">([^<]*)<\/a>";
+my %list = ();
 
 spider($url);
 
 sub spider
 {
-	# Method will take a single URL as a parameter.
-	# Create array of links with get($_) that follow $pattern.
-	# Attempt to save all the links in the hash for easy lookup
-	# spider($key) unless exists $map{$key} 
+	#Method will take a single URL as a parameter.
+	my $seed = $_[0];
+	
+	#Get page and store it
+	#Problem: recursively, doesn't the page stay in memory for the entire spider?
+	#There might be memory problem with recursive method?
+	my $page = get($seed);
+	
+	#Find all valid links in the page and add it to our map.
+	#Major problem: theres a TON of pages..must consider not getting Adpages, only names of ads
+	while($page =~ /$pattern/gi)
+	{
+	    if(!exists $list{$1})
+	    {
+	    	$list{$1} = $2;
+	    	print $1." ".$2."\n";
+			spider($1);
+	    }
+	}
 }
 
 sub is_listing
