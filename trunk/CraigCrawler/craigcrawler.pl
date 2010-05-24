34eed8a6256e3df6696f72647b2d0dc72ef5f4c3
Rodrigo Salazar <rodrodsalazar@gmail.com>
2010-05-23 23:26:40 -0400

    craigcrawler.pl: Made some progress on what to spider and what not to spider, but broke the spider in the process. 	Added some very messy code.
    git-svn-id: http://prmf.googlecode.com/svn@362 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/CraigCrawler/craigcrawler.pl
+++ trunk/CraigCrawler/craigcrawler.pl
@@ -5,35 +5,59 @@ use strict;
 use LWP::Simple;
 
 my $url = "http://craigslist.org";
-my $pattern = "<a href=\"(http:\/\/[^\"]*\.craigslist\.org\/(?!about)[^\"]*)\">([^<]*)<\/a>";
+my $pattern = "<a href=\"([^\"]*)\">([^<]*)<\/a>";
+# example listing http://miami.craigslist.org/brw/act/1755874712.html , not catching it..
 my %list = ();
+my $max_depth = 10;
 
-spider($url);
+spider($url,0);
+print scalar keys %list," found";
 
 sub spider
 {
 	#Method will take a single URL as a parameter.
 	my $seed = $_[0];
+	my $depth = $_[1];
 	
 	#Get page and store it
-	#Problem: recursively, doesn't the page stay in memory for the entire spider?
-	#There might be memory problem with recursive method?
-	my $page = get($seed);
+	my $page = get($seed) or print "Could not download page ".$url."\n";
 	
 	#Find all valid links in the page and add it to our map.
-	#Major problem: theres a TON of pages..must consider not getting Adpages, only names of ads
 	while($page =~ /$pattern/gi)
 	{
 	    if(!exists $list{$1})
 	    {
 	    	$list{$1} = $2;
-	    	print $1." ".$2."\n";
-			spider($1);
+	    	print $depth." ".$seed." => ".$1." ".$2;
+	    	if($depth > $max_depth || dont_spider($1))
+	    	{
+				print " - not spidered\n";
+	    	}
+	    	else
+	    	{
+	    		print "\n";
+	    		if($1 =~ /http/)
+	    		{
+	    			spider($1,$depth+1);
+	    		}
+	    		else
+	    		{
+	    			spider($seed."/".$1,$depth+1);
+	    		}
+	    	}
 	    }
 	}
+	
 }
 
-sub is_listing
+sub dont_spider
 {
-	# Determine if given page is a listing
+	#List of types of pages not to spider, first one is listings (spidering includes downloading)
+	my @bad = ( qr/(\d+\.html)$/ , qr/blog/ , qr/\/about\//, qr/^https:/, qr/http:\/\/.*\.(?!craigslist.org)/ );
+	my $ret = 0;
+	foreach my $badpattern (@bad)
+	{
+		$ret = 1 if $_[0] =~ /$badpattern/;
+	}
+	return $ret;
 }
