0279ac52ffade9dc60f62a380715a817e785a445
Rodrigo Salazar <rodrodsalazar@gmail.com>
2010-05-24 14:51:37 -0400

    craigcrawler.pl: Added a statement to remove leading slashes in directory links. Attempted to change one of the 'bad' patterns, currently a broken pattern which doesn't 'compile'.
    git-svn-id: http://prmf.googlecode.com/svn@363 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/CraigCrawler/craigcrawler.pl
+++ trunk/CraigCrawler/craigcrawler.pl
@@ -5,8 +5,7 @@ use strict;
 use LWP::Simple;
 
 my $url = "http://craigslist.org";
-my $pattern = "<a href=\"([^\"]*)\">([^<]*)<\/a>";
-# example listing http://miami.craigslist.org/brw/act/1755874712.html , not catching it..
+my $pattern = qr/<a href=\"([^\"]*)\">([^<]*)<\/a>/;
 my %list = ();
 my $max_depth = 10;
 
@@ -25,24 +24,30 @@ sub spider
 	#Find all valid links in the page and add it to our map.
 	while($page =~ /$pattern/gi)
 	{
-	    if(!exists $list{$1})
+		my $curr_link = $1;
+		my $page_name = $2;
+	    if(!exists $list{$curr_link})
 	    {
-	    	$list{$1} = $2;
-	    	print $depth." ".$seed." => ".$1." ".$2;
-	    	if($depth > $max_depth || dont_spider($1))
+	    	$list{$curr_link} = $page_name;
+	    	print $depth." ".$seed." => ".$curr_link." ".$page_name;
+	    	if($depth > $max_depth || dont_spider($curr_link))
 	    	{
 				print " - not spidered\n";
 	    	}
 	    	else
 	    	{
 	    		print "\n";
-	    		if($1 =~ /http/)
+	    		if($curr_link =~ /http/)
 	    		{
-	    			spider($1,$depth+1);
+	    			spider($curr_link,$depth+1);
 	    		}
-	    		else
+	    		else #handle links that are subdirectories
 	    		{
-	    			spider($seed."/".$1,$depth+1);
+	    			if($curr_link =~ /^\/(.*)/) #remove leading slash
+	    			{
+	    				$curr_link = $1;
+	    			}
+	    				spider($seed."/".$1,$depth+1);
 	    		}
 	    	}
 	    }
@@ -53,11 +58,17 @@ sub spider
 sub dont_spider
 {
 	#List of types of pages not to spider, first one is listings (spidering includes downloading)
-	my @bad = ( qr/(\d+\.html)$/ , qr/blog/ , qr/\/about\//, qr/^https:/, qr/http:\/\/.*\.(?!craigslist.org)/ );
+	my @bad = (  qr/(\d+\.html)$/ ,
+				 qr/blog/ , 
+				 qr/\/about\//, 
+				 qr/^https:/, 
+				 qr/?(^(http))(?!(\.craigslist\.org))|(?!(^\/))/ ,#broken, trying to match only craigslist sites and local directories
+				 qr/\/forums\//
+				  );
 	my $ret = 0;
 	foreach my $badpattern (@bad)
 	{
-		$ret = 1 if $_[0] =~ /$badpattern/;
+		$ret = 1 if $_[0] =~ /$badpattern/i && print " ".$badpattern;
 	}
 	return $ret;
 }
