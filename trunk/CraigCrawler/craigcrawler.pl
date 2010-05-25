2ef739df3e3312e39d8bd0e8a39d69d88e5445c9
Rodrigo Salazar <rodrodsalazar@gmail.com>
2010-05-24 20:57:47 -0400

    craigcrawler.pl: Erased dont_spider method because I decided its a horrible way to check for bad links. It might be better just to be more specific with our original pattern.. Added a list of problems to tackle!
    git-svn-id: http://prmf.googlecode.com/svn@366 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/CraigCrawler/craigcrawler.pl
+++ trunk/CraigCrawler/craigcrawler.pl
@@ -4,10 +4,16 @@ use warnings;
 use strict;
 use LWP::Simple;
 
+#Current problems: (In order of priority)
+#1.Main regex pattern is bad, it doesnt filter out actual adpages, blog.craigslist,craigslist.org/ABOUT, etc..
+#2.organization of data gathered 
+#3.Very slow, need to possibly have a few threads downloading at once
+#4.code is not very slick
+
 my $url = "http://craigslist.org";
-my $pattern = qr/<a href=\"([^\"]*)\">([^<]*)<\/a>/;
+my $pattern = qr/<a href=\"([^\"]*craigslist.org[^\"]*)\">([^<]*)<\/a>/;
 my %list = ();
-my $max_depth = 10;
+my $max_depth = 8;
 
 spider($url,0);
 print scalar keys %list," found";
@@ -19,7 +25,7 @@ sub spider
 	my $depth = $_[1];
 	
 	#Get page and store it
-	my $page = get($seed) or print "Could not download page ".$url."\n";
+	my $page = get($seed) or print "Could not download page ".$seed."\n" && return;
 	
 	#Find all valid links in the page and add it to our map.
 	while($page =~ /$pattern/gi)
@@ -29,16 +35,15 @@ sub spider
 	    if(!exists $list{$curr_link})
 	    {
 	    	$list{$curr_link} = $page_name;
-	    	print $depth." ".$seed." => ".$curr_link." ".$page_name;
-	    	if($depth > $max_depth || dont_spider($curr_link))
+	    	if($depth <= $max_depth)
 	    	{
-				print " - not spidered\n";
-	    	}
-	    	else
-	    	{
-	    		print "\n";
+	    		print $depth." ".$seed." => ".$curr_link." ".$page_name."\n";
 	    		if($curr_link =~ /http/)
 	    		{
+	    			if($curr_link =~ /(.*)\/$/) #remove trailing slashes in full URLs
+	    			{
+	    				$curr_link = $1;
+	    			}
 	    			spider($curr_link,$depth+1);
 	    		}
 	    		else #handle links that are subdirectories
@@ -47,6 +52,7 @@ sub spider
 	    			{
 	    				$curr_link = $1;
 	    			}
+	    			
 	    				spider($seed."/".$1,$depth+1);
 	    		}
 	    	}
@@ -55,20 +61,3 @@ sub spider
 	
 }
 
-sub dont_spider
-{
-	#List of types of pages not to spider, first one is listings (spidering includes downloading)
-	my @bad = (  qr/(\d+\.html)$/ ,
-				 qr/blog/ , 
-				 qr/\/about\//, 
-				 qr/^https:/, 
-				 qr/?(^(http))(?!(\.craigslist\.org))|(?!(^\/))/ ,#broken, trying to match only craigslist sites and local directories
-				 qr/\/forums\//
-				  );
-	my $ret = 0;
-	foreach my $badpattern (@bad)
-	{
-		$ret = 1 if $_[0] =~ /$badpattern/i && print " ".$badpattern;
-	}
-	return $ret;
-}
