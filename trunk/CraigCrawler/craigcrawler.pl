6a14476c03bc72f4870a49e4bbaecbf3db444275
Rodrigo Salazar <rodrodsalazar@gmail.com>
2010-05-29 17:48:55 -0400

    craigcrawler.pl: Fixed bug with multiple slashes appearing. Spider no longer visits ad-listings so to scan faster. 	All ad-links and ad-descriptions are stored in a separate table now. Added a basic pattern to match ad listings.
    git-svn-id: http://prmf.googlecode.com/svn@373 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/CraigCrawler/craigcrawler.pl
+++ trunk/CraigCrawler/craigcrawler.pl
@@ -5,19 +5,21 @@ use strict;
 use LWP::Simple;
 
 #Current problems: (In order of priority)
-#1.Program needs to be examined to see if it really does visit all webpages intended..it does seem to.
-#2.Very slow, need to possibly have a few threads downloading at once .. project will get more interesting as well
-#3.organization of data gathered, for example: do we need to download AD-pages or only their descriptions?
+#1.Very slow, need to possibly have a few threads downloading at once .. project will get more interesting as well
+#2.Organization of data gathered, currently the links are separated as only ads or not ads.
+#3.Need more control over scanning, possibly the option to scan as far as 2 pages back, or even by day.
 #4.code is not very slick
 
 my $url = "http://craigslist.org";
 my $link_pattern = qr/<a href=\"([^\"]*)\">([^<]*)<\/a>/;
 
 #TODO: need to exclude very old listings
-my @bad_patterns = ( qr/mailto/, qr/https/, qr/blog\./, qr/\/about\//, qr/forums\./, qr/cgi-bin/);
+my @bad_patterns = ( qr"^/$", qr/mailto/, qr/https/, qr/blog\./, qr"/about/", qr"^(cal)", qr/forums[\/\.]?/, qr/cgi-bin/, qr/(index[0-9]+\.html)$/);
 my @good_patterns = ( qr/craigslist\.org/, qr/^(?!http)/ );
+my $listing_pattern = qr"(/[0-9]+.html)$";
 
-my %list = ();
+my %list = (); #Will contain all links spidered
+my %ads = ();  #Will contain all advertisement links
 my $max_depth = 10;
 
 spider($url,"craigslist",0);
@@ -42,13 +44,20 @@ sub spider
 	{
 		my $curr_link = $1;
 		my $page_name = $2;
-		next scan if(validate_link($curr_link));
+		next scan if(!validate_link($curr_link));
 		
 		$curr_link = clean_link($seed,$curr_link);
 		
     	next scan if(exists $list{$curr_link});
     	print $depth." ".$seed." => ".$curr_link." ".$page_name."\n";
-    	spider($curr_link,$page_name,$depth+1);
+    	if ($curr_link =~ /$listing_pattern/)
+    	{
+    		$ads{$curr_link} = $page_name;
+    	}
+    	else 
+    	{
+			spider($curr_link,$page_name,$depth+1);
+    	}
 	}
 }
 
@@ -57,36 +66,30 @@ sub validate_link
 		my $curr_link = $_[0];
 		foreach my $bad (@bad_patterns)
 		{
-			return 1 if($curr_link =~ /$bad/);
+			return 0 if($curr_link =~ /$bad/);
 		}
 		
 		foreach my $good (@good_patterns)
 		{
-			return 0 if($curr_link =~ /$good/);
+			return 1 if($curr_link =~ /$good/);
 		}
-		return 1;
+		return 0;
 }
 
 sub clean_link
 {
-		#this method has a bug, with double slashes appearing in links after its done
-		#causing double downloads , one for link with single slash, and one with double
 		my $seed = $_[0];
 		my $curr_link = $_[1];
 		
 	    if($curr_link =~ /http/)
     	{
-    		if($curr_link =~ /(.*)\/$/) #remove trailing slashes in full URLs, BROKEN..causes double slash?
-    		{
-    			$curr_link = $1;
-    		}
+    		$curr_link =~ s"/$""; #remove trailing slashes 
     	}
     	else
     	{
-	    	if($curr_link =~ /^\/?(\w*)\/?/) #get subdirectory name, without slashes
-	    	{
-	    		$curr_link = "/".$1; #append slash to create a full URL with the seed.
-	    	}
+
+	    	$curr_link =~ s"/""; #replace slashes with empty string, and append a single slash to front
+	    	$curr_link = "/".$curr_link;
 	    	$curr_link = $seed.$curr_link;
     	}
     	return $curr_link;
