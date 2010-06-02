98a387d46c8efa0d208a1bc1f2f177bc13736e1d
Rodrigo Salazar <rodrodsalazar@gmail.com>
2010-06-02 14:57:10 -0400

    File was not added to commit list in the previous commit.
    * pattern.pm: created to hold patterns and get methods.
    
    
    git-svn-id: http://prmf.googlecode.com/svn@383 93ad656a-1fb5-1e14-4869-072bfc7f1330

---
+++ trunk/CraigCrawler/pattern.pm
@@ -0,0 +1,27 @@
+package pattern;
+
+my @bad_patterns = ( qr"^/$", qr/mailto/, qr/https/, qr/blog\./, qr"/about/", qr"^(/?cal)", qr/forums[\/\.]?/, qr/cgi-bin/, qr/(index[0-9]+\.html)$/);
+my @good_patterns = ( qr/craigslist\.org/, qr/^(?!http)/ );
+my $link_pattern = qr/<a href=\"([^\"]*)\">([^<]*)<\/a>/;
+my $listing_pattern = qr"(/[0-9]+.html)$";
+
+sub get_listing_pattern
+{
+	return $listing_pattern;
+}
+
+sub get_link_pattern
+{
+	return $link_pattern;
+}
+
+sub get_bad_patterns
+{
+	return @bad_patterns;
+}
+
+sub get_good_patterns
+{
+	return @good_patterns;
+}
+1;
