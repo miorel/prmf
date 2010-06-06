35d192b8be7a878fd855511acdb056e57e2296d9
Rodrigo Salazar <rodrodsalazar@gmail.com>
2010-06-05 20:49:54 -0400

    * craigcrawler.pl: Updated TODO list with new goals for the project.
    git-svn-id: http://prmf.googlecode.com/svn@392 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- trunk/CraigCrawler/craigcrawler.pl
+++ trunk/CraigCrawler/craigcrawler.pl
@@ -9,8 +9,9 @@ require "basic_spider.pl";
 require "threaded_spider.pl";
 
 #Current Issues: (In order of priority)
-#1.Get websites downloading using multiple threads, threads are locking up.. not sure if being done properly.
-#2.File output for easier testing.
+#1.File output for easier testing.
+#2.Need to refine the code for the multithreaded spider, specifically the end conditions on the threads..
+#3.SOCKS Proxy support + Nice-Bot support (Delays, etc.)
 #3.Organization of data gathered, currently the links are separated as only ads or not ads.
 #4.Need more control over scanning, possibly the option to scan as far as 2 pages back, or even by day.
 #5.Code is not very slick.
@@ -20,7 +21,7 @@ my $site_name = "craigslist";
 
 if ($Config{useithreads} && settings::threading_on() ) 
 {
-	threaded_spider::threaded_spider($url,$site_name,0);
+	threaded_spider::threaded_spider($url,$site_name);
 }
 else
 {
