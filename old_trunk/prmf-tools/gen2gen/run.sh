913acb54c7b9d87ada808027de743677cb9eead4
Miorel-Lucian Palii <mlpalii@gmail.com>
2010-11-09 13:12:22 -0400

    First acceptable version of gen2gen is finally done.
    git-svn-id: http://prmf.googlecode.com/svn@463 93ad656a-1fb5-1e14-4869-072bfc7f1330

--- old_trunk/prmf-tools/gen2gen/run.sh
+++ old_trunk/prmf-tools/gen2gen/run.sh
@@ -1,10 +1,17 @@
 run () {
+	destination=hi
+
 	parse_opts=1
 	operand=()
+	opt=()
 	for arg in $@; do
 		parsed=0
 		if (( $parse_opts )); then
 			case "$arg" in
+			--conf)
+				parsed=1
+				opt[${#opt[*]}]="conf"
+			;;
 			--version|-v*)
 				parsed=1
 				print_version
@@ -29,10 +36,38 @@ run () {
 		fi
 		(( $parsed )) || operand[${#operand[*]}]=$arg
 	done
-	if [ ${#operand[*]} -gt 0 ]; then
-		extra_operand ${operand[0]}
+
+	j=0
+	for i in `seq 0 $(( ${#opt[*]} - 1 ))`; do
+		arg=${opt[i]}
+		case "$arg" in
+		conf)
+			exec 3<&0
+			file=${operand[$j]}
+			if [ "$file" != "-" ]; then
+				exec < "$file"
+			fi
+			while read line; do
+				case "$line" in
+				destination=*)
+					key=${line%%=*}
+					val=${line#*=}
+					eval "$key=\$val"
+				;;
+				esac
+			done
+			exec 0<&3 3<&-
+			j=$(( $j + 1 ))
+		;;
+		esac
+	done
+
+	if [ ${#operand[*]} -gt $j ]; then
+		extra_operand ${operand[$j]}
 		return 1
 	fi
+
+	echo "Destination = $destination"
 
 	wd=$PWD
 	tmp=`mktemp -d -p .`
@@ -40,7 +75,7 @@ run () {
 	_extract
 	source util.sh
 	source mkgentoo.sh
-	_mkgentoo /dev/sda
+	_mkgentoo $destination
 	ret=$?
 	cd $wd
 	rm -rf $tmp
@@ -48,30 +83,31 @@ run () {
 }
 
 print_version () {
-	cat << '_VERSION_'
-gen2gen 0.0.1-r462 (November 8, 2010)
+	cat << EOF
+gen2gen 0.0.1 (November 9, 2010)
 Copyright (C) 2010 Miorel-Lucian Palii
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
-_VERSION_
+EOF
 }
 
 print_help () {
-	cat << _HELP_
+	cat << EOF
 Usage: $0 [OPTION]...
 Install a basic Gentoo system.
 
-      --          suppress further option parsing (in case you need to pass
-                  operands that start with a hyphen without confusing them for
-                  options)
-  -h, --help, -?  display this help and exit
-  -v, --version   output version information and exit
+      --conf FILE  read configuration from FILE (may be \`-' for stdin)
+      --           suppress further option parsing (in case you need to pass
+                   operands that start with a hyphen without confusing them for
+                   options)
+  -h, --help, -?   display this help and exit
+  -v, --version    output version information and exit
 
 Exit status:
  0  if OK,
     otherwise nonzero
-_HELP_
+EOF
 }
 
 suggest_help () {
