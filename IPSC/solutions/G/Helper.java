35fec32d424d0a1627d672f0c8e9083984253244
Joseph Thuemler <joethuemler@gmail.com>
2011-06-19 03:12:25 -0400

    added helper files, input, and final output for G
    git-svn-id: http://prmf.googlecode.com/svn@509 93ad656a-1fb5-1e14-4869-072bfc7f1330

---
+++ IPSC/solutions/G/Helper.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Helper {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int i=0;
+		while(++i<=1530000) in.nextLine();
+		while(in.hasNext()) System.out.println(in.nextLine());
+	}
+}
